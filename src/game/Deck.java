adf928de374f3155de98e683e79949f439e7a539
groelsmt <groelsmt@eos28.cis.gvsu.edu>
2017-10-02 16:09:24 -0400

    Test

--- src/game/Deck.java
+++ src/game/Deck.java
@@ -13,12 +13,20 @@ public class Deck {
 	 * 
 	 */
 	private ArrayList<Card> cards;
+	/**
+	 * 
+	 */
+	private String[] cardNames = {"One", "Two", "Three"};
 	
 	/**
-	 * Initializes a standard 52 deck of cards
+	 * Initializes a standard 52 deck of cards.
 	 */
 	public Deck() {
 		//
+		Suit curSuit = Suit.HEARTS;
+		for (int i = 1; i < 14; i++) {
+			Card curCard = new Card("", i, curSuit);
+		}
 	}
 	
 	/**
