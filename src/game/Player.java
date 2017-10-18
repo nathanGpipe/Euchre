3ba5952903511aa548325e31834aa86441c965c9
Saxton <Saxton@DESKTOP-EBNHLDO>
2017-10-17 20:32:43 -0400

    Added some things to Player class and am working on its test cases.

--- src/game/Player.java
+++ src/game/Player.java
@@ -3,8 +3,8 @@ package game;
 import java.util.ArrayList;
 
 /**
- * @author Nathan Pipe, Tressa Groelsma, Saxton Stafford Class to store data
- *         pertaining to each player.
+ * @author Nathan Pipe, Tressa Groelsma, Saxton Stafford 
+ * Class to store data pertaining to each player.
  */
 public class Player {
 
@@ -33,6 +33,91 @@ public class Player {
 	 * @return The desired suit to be trump, or NULL if no choice.
 	 */
 	public Suit chooseTrump() {
+		return getFavoriteSuit();
+	}
+
+	/**
+	 * Chooses whether or not to have the dealer pick up the initial card.
+	 * 
+	 * @param c The card to check for the dealer to pick up.
+	 * @return True if the player wants them to pick it up.
+	 */
+	public boolean pickUp(final Card c) {
+		if (c.getSuit() == getFavoriteSuit()) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Swaps the card on the top of the discard pile with 
+	 * one in the player's hand.
+	 * 
+	 * @param c
+	 *            The card on top of the discard pile.
+	 */
+	public void swap(final Card c) {
+		
+	}
+
+	/**
+	 * Looks at what cards are on the table and chooses which card to play.
+	 * 
+	 * @param table
+	 *            An ArrayList of cards that have been played this trick.
+	 * @param trump
+	 *            The trump suit of this round.
+	 * @return A card from the player's hand that they choose to play.
+	 */
+	public Card choosePlay(final ArrayList<Card> table, final Suit trump) {
+		int index = 0;
+		return hand.remove(index);
+	}
+
+	/**
+	 * Adds a card to the player's hand.
+	 * 
+	 * @param c
+	 *            The card to be added to the hand.
+	 */
+	public void addToHand(final Card c) {
+		hand.add(c);
+	}
+
+	/**
+	 * Accessor method for hand.
+	 * 
+	 * @return ArrayList of the cards in the players hand.
+	 */
+	public ArrayList<Card> getHand() {
+		return hand;
+	}
+
+	/**
+	 * Sets the partner for this player and sets the partner for 
+	 * the Player in the parameter's partner to this Player.
+	 * @param p
+	 *            The partner of the player.
+	 */
+	public void setPartner(final Player p) {
+		partner = p;
+		p.partner = this;
+	}
+
+	/**
+	 * Accessor method for partner instance variable.
+	 * 
+	 * @return Partner of the player.
+	 */
+	public Player getPartner() {
+		return partner;
+	}
+	
+	/**
+	 * Private helper method to assist in AI functions.
+	 * @return the preferred suit, given the player's hand
+	 */
+	private Suit getFavoriteSuit() {
 		int numClubs = 0;
 		int numSpades = 0;
 		int numDiamonds = 0;
@@ -91,79 +176,5 @@ public class Player {
 		} else {
 			return null;
 		}
-	}
-
-	/**
-	 * Chooses whether or not to have the dealer pick up the initial card.
-	 * 
-	 * @param c The card to check for the dealer to pick up.
-	 * @return True if the player wants them to pick it up.
-	 */
-
-	public boolean pickUp(final Card c) {
-		return false;
-	}
-
-	/**
-	 * Swaps the card on the top of the discard pile with 
-	 * one in the player's hand.
-	 * 
-	 * @param c
-	 *            The card on top of the discard pile.
-	 */
-	public void swap(final Card c) {
-
-	}
-
-	/**
-	 * Looks at what cards are on the table and chooses which card to play.
-	 * 
-	 * @param table
-	 *            An ArrayList of cards that have been played this trick.
-	 * @param trump
-	 *            The trump suit of this round.
-	 * @return A card from the player's hand that they choose to play.
-	 */
-	public Card choosePlay(final ArrayList<Card> table, final Suit trump) {
-		int index = 0;
-		return hand.remove(index);
-	}
-
-	/**
-	 * Adds a card to the player's hand.
-	 * 
-	 * @param c
-	 *            The card to be added to the hand.
-	 */
-	public void addToHand(final Card c) {
-		hand.add(c);
-	}
-
-	/**
-	 * Accessor method for hand.
-	 * 
-	 * @return ArrayList of the cards in the players hand.
-	 */
-	public ArrayList<Card> getHand() {
-		return hand;
-	}
-
-	/**
-	 * Sets the partner for this player.
-	 * 
-	 * @param p
-	 *            The partner of the player.
-	 */
-	public void setPartner(final Player p) {
-		partner = p;
-	}
-
-	/**
-	 * Accessor method for partner instance variable.
-	 * 
-	 * @return Partner of the player.
-	 */
-	public Player getPartner() {
-		return partner;
 	}
 }
---
+++ src/game/PlayerTest.java
@@ -0,0 +1,104 @@
+package game;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+/**
+ * JUnit test cases for the Player class.
+ * @author Nathan Pipe, Tressa Groelsma, Saxton Stafford
+ */
+public class PlayerTest {
+
+
+	Player p1 = new Player();
+	Player p2 = new Player();
+	
+
+	Card c1 = new Card("King", 13, Suit.HEARTS);
+	Card c2 = new Card("Jack", 11, Suit.HEARTS);
+	Card c3 = new Card("Jack", 11, Suit.DIAMONDS);
+	Card c4 = new Card("Nine", 9, Suit.CLUBS);
+	Card c5 = new Card("Ten", 10, Suit.SPADES);
+	
+	ArrayList<Card> hand1 = new ArrayList<Card>();
+	
+	
+
+	/**
+	 * Test method for {@link game.Player#chooseTrump()}.
+	 */
+	@Test
+	public void testChooseTrump1() {
+		for (int i = 0; i < hand1.size(); i++) {
+			p1.addToHand(hand1.get(i));
+		}
+		AssertEquals("Failed to choose proper trump", Suit.HEARTS, 
+				p1.chooseTrump());
+	}
+
+	/**
+	 * Test method for {@link game.Player#pickUp(game.Card)}.
+	 */
+	@Test
+	public void testPickUp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link game.Player#swap(game.Card)}.
+	 */
+	@Test
+	public void testSwap() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for 
+	 * {@link game.Player#choosePlay(java.util.ArrayList, game.Suit)}.
+	 */
+	@Test
+	public void testChoosePlay() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link game.Player#addToHand(game.Card)}.
+	 */
+	@Test
+	public void testAddToHand() {
+		p1.addToHand(c1);
+		ArrayList<Card> hand = p1.getHand();
+		assertTrue("Failed to add to hand.", hand.contains(c1));
+	}
+
+	/**
+	 * Test method for {@link game.Player#getHand()}.
+	 */
+	@Test
+	public void testGetHand() {
+		p1.addToHand(c1);
+		ArrayList<Card> hand = p1.getHand();
+		assertTrue("Failed to get hand.", hand.contains(c1));
+	}
+
+	/**
+	 * Test method for {@link game.Player#setPartner(game.Player)}.
+	 */
+	@Test
+	public void testSetPartner() {
+		p1.setPartner(p2);
+		assertEquals(p1.getPartner(), p2);
+	}
+
+	/**
+	 * Test method for {@link game.Player#getPartner()}.
+	 */
+	@Test
+	public void testGetPartner() {
+		p1.setPartner(p2);
+		assertEquals(p1.getPartner(), p2);
+	}
+
+}
